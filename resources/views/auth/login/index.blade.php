3d6cfb6dd62ae3dc7c67483fab997062d1a73d32
hippies67 <ikbal.malik26@gmail.com>
2022-04-25 09:22:54 +0700

    perubahan

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -53,11 +53,11 @@
       <div class="container mt-5">
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-            <div class="login-brand p-1" style="background-color: #ffffff !important;border-radius: 15px;">
+            {{-- <div class="login-brand p-1" style="background-color: #ffffff !important;border-radius: 15px;">
               @foreach($web as $webs)
               <a href="{{ url('/') }}"><img src="{{ Storage::url($webs->logo) }}" alt="logo" width="100"></a>
               @endforeach
-            </div>
+            </div> --}}
 
             <div class="card" style="border-top: 2px solid {{ $primary_color }}; border-radius: 15px;">
               <div class="card-header">
